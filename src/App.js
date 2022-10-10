a094bd4a719a14c784da20871402ef232c6f93c6
Behnam <behnam.amiri@gitkraken.com>
2022-10-10 15:19:45 -0700

    Basic stuff

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+
+  const name = 'Behnam';
+
+  return (
+    <div className="container">
+      <h1>Hello From ReactJs from {name}</h1>
+    </div>
+  );
+}
+
+export default App;
