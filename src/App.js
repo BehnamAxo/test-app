94dc0d5c7af61b27828c756b653201899b69205e
Behnam <behnam.amiri@gitkraken.com>
2022-10-13 09:49:54 -0700

    Add task to the list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
-const App = () => (
-  <div className="container">
-    <Header />
-  </div>
-);
+const App = () => {
+  const [tasks, setTasks] = useState([
+      {
+          id: 1,
+          text: 'Doctors Appointment',
+          day: 'Feb 6th at 2:44 p.m.',
+          reminder: true
+      },
+      {
+          id: 2,
+          text: 'Meeting  with Dr. Douglas Shepherd',
+          day: 'Feb 8th at 4:30 p.m.',
+          reminder: false
+      },
+      {
+          id: 3,
+          text: 'Grocery Shopping',
+          day: 'Feb 12th at 8:30 a.m.',
+          reminder: false
+      },
+      {
+          id: 4,
+          text: 'Gym time',
+          day: 'Feb 21st at 6:00 a.m.',
+          reminder: true
+      }
+  ]);
+
+  return (
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks}/>
+    </div>
+  );
+}
 
 export default App;
 
---
+++ src/components/Tasks.js
@@ -0,0 +1,9 @@
+import Task from "./Task";
+
+const Tasks = ({ tasks }) => 
+    <>
+        { tasks.map((task) => ( <Task key={task.id} task={task} /> ))}
+    </>
+
+
+export default Tasks;
